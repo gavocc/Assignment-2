017f9fbbdc0a9c7fd2994af512aa23311ffc9efe
gavocc <55485811+gavocc@users.noreply.github.com>
2019-09-30 23:10:52 +1000

    Delete leftmenu.php

--- leftmenu.php
+++
@@ -1,5 +0,0 @@
-<?php
-    echo "<a href='index.php/?page=index'>Front Page</a>"; // set page to index
-    echo "<a href='index.php/?page=about'>About</a>";      // page = about
-    echo "<a href='index.php/?page=contact'>Contact</a>";  // page = contact
-?>
